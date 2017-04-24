9f7ea9f483179ffce4ebf554828203cee463f80b
Fernando Martinez Larrañaga <fernandomartinezlarranaga@MacBook-Air-de-Fernando.local>
2017-04-24 18:25:08 +0200

    Primer intento

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,53 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.currentDirection= "up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.elevatorInterval = setInterval(() => {
+      this.update();
+    }, 1000)
+  }
+
+
+  stop() {
+    clearInterval (this.elevatorInterval);
+  }
+
+  update() {
+    log();
+
+  }
+
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR){
+      this.floor +=1;
+      this.currentDirection= "up";
+    }
+
+  }
+
+
+  floorDown() {
+    if (this.floor > 0){
+      this.floor -=1;
+      this.currentDirection="down";
+
+    }
+   }
+
+
   call() { }
-  log() { }
+
+  log() {
+
+    console.log( `Direction: ${this.currentDirection} | Floor: ${this.floor}` );
+
+  }
 }
 
 module.exports = Elevator;
